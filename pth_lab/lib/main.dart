4f7e1690e5fbfa1c2cb70bfb6e0d2c42ebec6556
Bruce <kmisanotaku@gmail.com>
2018-12-26 10:07:33 +0630

    Submit for the home page UI and some modification

--- pth_lab/lib/main.dart
+++ pth_lab/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() => runApp(PTHLAB());
 
@@ -63,18 +64,21 @@ class _LoginPageState extends State<LoginPage> {
             ),
             Padding(
               padding: new EdgeInsets.fromLTRB(70, 5, 70, 5),
-              child: RaisedButton(
-                child: const Text('LOGIN',),
-                color: Theme.of(context).accentColor,
-                textColor: Colors.white,
-                splashColor: Colors.blueGrey,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-              ),
+              child: new SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  child: const Text('LOGIN',),
+                  color: Theme.of(context).accentColor,
+                  textColor: Colors.white,
+                  splashColor: Colors.blueGrey,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                ),
+              )
             )
           ],
         ),
@@ -88,15 +92,237 @@ class HomePage extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("PTH LAB"),
+       drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("PTH Lab"),
+              currentAccountPicture: Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+              accountEmail: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.home,color: Colors.deepOrange,),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.code,color: Colors.deepOrange,),
+              title: Text('Courses'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.feedback,color: Colors.deepOrange,),
+              title: Text('Feedback'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.share,color: Colors.deepOrange,),
+              title: Text('Share'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.monetization_on,color: Colors.deepOrange,),
+              title: Text('Donate'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('data')
+      appBar: AppBar(
+        title: Text("Welcome from PTH LAB"),
+        actions: <Widget>[ 
+          new IconButton( 
+            icon: new Icon(Icons.search,color: Colors.white),
+            tooltip: 'Search', 
+            onPressed: () {
+              return showDialog(
+                context: context,
+                builder: (context){
+                  return AlertDialog(
+                    content: Text("တည်ဆောက်နေဆဲ"),
+                  );
+                }
+              );
+            },
+          ),
+        ], 
+      ),
+      body: Container(
+        child: new SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: new BoxConstraints(),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  child: new SizedBox(
+                    height: 200.0,
+                    child: new Carousel(
+                      images: [
+                        new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+                        new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+                        new ExactAssetImage("assets/images/LaunchImage.jpg")
+                      ],
+                      showIndicator: false,
+                      borderRadius: false,
+                      moveIndicatorFromBottom: 180.0,
+                      noRadiusForIndicator: true,
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: new EdgeInsets.all(20),
+                  child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: new Container(
+                      child: Row(
+                        children: <Widget>[
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  )
+                ),
+                Padding(
+                  padding: new EdgeInsets.all(20),
+                  child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: new Container(
+                      child: Row(
+                        children: <Widget>[
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  )
+                ),
+                Padding(
+                  padding: new EdgeInsets.all(20),
+                  child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: new Container(
+                      child: Row(
+                        children: <Widget>[
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  )
+                ),
+                Padding(
+                  padding: new EdgeInsets.all(20),
+                  child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: new Container(
+                      child: Row(
+                        children: <Widget>[
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                          new SizedBox(
+                            width: MediaQuery.of(context).size.width/2-20,
+                            child: Center(
+                              child: new Column(
+                                children: <Widget>[
+                                  Image.network('https://www.empowervate.org/wp-content/uploads/2015/11/circle-300x300.jpg',height: 70,),
+                                  Text('data')
+                                ],
+                              )
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+        )
       ),
     );
   }
- 
 }
 
 
